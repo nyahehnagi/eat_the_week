882800195737d4505d72a9017e0b6b0fd9b6a590
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-04-01 13:24:12 +0100

    Express and MongoDB scaffold

---
+++ README.md
@@ -0,0 +1,41 @@
+# Eat The Week
+
+---
+
+## Tech Used
+
+- Javascript with Node
+- Jest for testing
+- Prettier for linting
+- MongoDB
+- Express
+- React
+
+## Installation
+
+---
+
+Install node via your favourite package manager if required, see [here](https://nodejs.org/en/ "Node") for more details
+
+Install packges
+
+```
+$> npm install
+```
+
+To install this code from the latest source
+
+```
+$> git clone git@github.com:nyahehnagi/eat_the_week.git
+
+```
+
+## Testing
+
+---
+
+To run jest tests
+
+```
+$> npx jest
+```
---
+++ app.js
@@ -0,0 +1,40 @@
+const createError = require("http-errors");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const path = require("path");
+const logger = require("morgan");
+const methodOverride = require("method-override");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// route setup - Insert routes here
+// e.g const postsRouter = require("./routes/posts");
+
+// Tell app to use the routes
+// e.g app.use("/posts", postsRouter);
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
