0e15ee6d27fad4cbe54155fb863fc51212395a44
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-04-02 22:54:48 +0100

    seeing if routes change to rest type routing classes with express routes - now with an s

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,8 @@ const App = () => {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/recipe/new" element={<CreateRecipe />} />
-        <Route path="/recipe/" element={<ShowRecipes />} />
+        <Route path="/recipes/new" element={<CreateRecipe />} />
+        <Route path="/recipes/" element={<ShowRecipes />} />
       </Routes>
     </div>
   );
