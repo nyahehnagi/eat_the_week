7f1bd6798336f0754d05486cd43e5b2db4fbbb49
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-04-04 15:58:19 +0100

    Can create user from front end

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,10 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+ 
 
 export default function Home() {
 
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    name: ""
+  });
+
+  const navigate = useNavigate();
+
+  // This method will update the state properties.
+  function updateForm(value) {
+   return setForm((prev) => {
+    return { ...prev, ...value };
+    });
+  }
+
+  // This function will handle the submission.
+ async function onSubmit(e) {
+  e.preventDefault();
+
+  // When a post request is sent to the create url, add a new record to the database.
+  const user = { ...form };
+   
+  await fetch("/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({user}),
+  })
+  .catch(error => {
+    window.alert(error);
+    return;
+  });
+
+  setForm({ email: "", password: "", name: ""});
+  navigate("/recipe");
+  }
+
   return (
     <div>
-      <h3>Eat The Week - Coming Soon!</h3>
+      <h3>Welcome to Eat The Week</h3>
+      <h3>Create User</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={form.email}
+            onChange={(e) => updateForm({ email: e.target.value })}
+          />
+        </div>
+       
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            value={form.password}
+            onChange={(e) => updateForm({ password: e.target.value })}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="name"
+            className="form-control"
+            id="name"
+            value={form.name}
+            onChange={(e) => updateForm({ name: e.target.value })}
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="submit"
+            id="create-user"
+            value="Create User"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
     </div>
   );
  }
