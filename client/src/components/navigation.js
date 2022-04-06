35fe537985429ccab9145b9bc32986c73c17dc27
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-04-06 14:10:22 +0100

    Added dark theme

--- client/src/components/navigation.js
+++ client/src/components/navigation.js
@@ -14,20 +14,20 @@ export default function Navigation() {
 
   return(
   <>
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Eat The Week</Navbar.Brand>
+        <Navbar.Brand href="/">Eat The Week</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/auth/">Login</Nav.Link>
             <Nav.Link onClick={handleClick}>Log Out</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown title="Get Cooking" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/recipe">My Recipes</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Weekly Planner</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3"></NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Edit Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
