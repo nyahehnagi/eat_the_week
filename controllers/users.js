ee63b364afa64c62697e9f37244261817a94f2b8
Ledders26 <benlhall584@hotmail.co.uk>
2022-04-07 23:05:34 +0100

    not allowing duplicate emails

--- controllers/users.js
+++ controllers/users.js
@@ -2,26 +2,30 @@ const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
 const UsersController = {
-  Create: (req, res) => {
-    const user = new User(req.body.user);
-
-    user.save((err, result) => {
-      if (err) {
-        res.status(500).send("Error registering new user please try again.");
-        throw err;
+  Create: async (req, res) => {
+    const userExists = await User.findOne({ email: req.body.user.email })
+    if (userExists) {
+      return res.status(401).json({ error: "email already in use"})
+    } else {
+        const user = new User(req.body.user);
+        user.save((err, result) => {
+        if (err) {
+          res.status(500).send("Error registering new user please try again.");
+          throw err;
+        }
+        
+        const body = { _id: user._id, email: user.email };
+        const token = jwt.sign({ user: body }, process.env.AUTH_KEY, {
+          expiresIn: "1h",
+        });
+        
+        const payload = { id: result._id, name: result.name };
+        
+        res.cookie("token", token);
+        res.cookie("name", result.name);
+        res.json(payload);
       }
-
-      const body = { _id: user._id, email: user.email };
-      const token = jwt.sign({ user: body }, process.env.AUTH_KEY, {
-        expiresIn: "1h",
-      });
-
-      const payload = { id: result._id, name: result.name };
-
-      res.cookie("token", token);
-      res.cookie("name", result.name);
-      res.json(payload);
-    });
+    )} 
   },
 };
 
