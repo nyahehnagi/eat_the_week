cceac7618fd28e2104612724876a103b6fd1ed11
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-04-07 09:17:47 +0100

    Moved ingredients to a parent container

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/home/index";
 import MyRecipes from "./components/recipes/myRecipes";
-import ShowIngredients from "./components/ingredients/index";
-import CreateIngredients from "./components/ingredients/create";
+import MyIngredients from "./components/ingredients/myIngredients";
+
 import Logon from "./components/auth/create";
 import Register from "./components/auth/register";
 import Navigation from "./components/navigation"
 
 import "bootstrap/dist/css/bootstrap.css";
+
 
 const App = () => {
   return (
@@ -18,8 +19,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipe/" element={<MyRecipes />} />
-        <Route path="/ingredient/" element={<ShowIngredients />} />
-        <Route path="/ingredient/create" element={<CreateIngredients />} />
+        <Route path="/ingredient/" element={<MyIngredients />} />
         <Route path="/auth/" element={<Logon />} />
         <Route path="/auth/register" element={<Register />} />
       </Routes>
