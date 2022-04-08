e0c49e17a6cecc746ec08a92ac6919997b92fb5f
Ledders26 <benlhall584@hotmail.co.uk>
2022-04-08 14:48:11 +0100

    added controller

---
+++ controllers/planners.js
@@ -0,0 +1,15 @@
+const Planner = require("../models/planner");
+
+const PlannerController = {
+  Show: (req, res) => {
+    Planner.find((err, plan) => {
+      if (err) {
+        throw err;
+      }
+      res.json(plan);
+    });
+  },
+
+};
+
+module.exports = PlannerController;
