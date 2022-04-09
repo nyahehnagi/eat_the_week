cf68bfd101bfc668a7481d4036809686f4e56b28
Ledders26 <benlhall584@hotmail.co.uk>
2022-04-09 19:37:25 +0100

    pulling in updated main

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,8 @@ import MyUsers from "./components/users/myUsers";
 import Logon from "./components/auth/logon";
 import Register from "./components/auth/register";
 import Navigation from "./components/navigation";
-
+import Footer from "./components/footer"
+import "./components/styles.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 const App = () => {
@@ -17,19 +18,35 @@ const App = () => {
 
   return (
     <>
-      <div className="App">
+      <div className="App" >
         <h3>{user.name}</h3>
         <Navigation user={user} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/category/" element={<ShowCategories />} />
-          <Route path="/category/create" element={<CreateCategories />} />
-          <Route path="/recipe/" element={<MyRecipes />} />
-          <Route path="/ingredient/" element={<MyIngredients />} />
-          <Route path="/auth/" element={<Logon />} />
-          <Route path="/auth/register" element={<Register />} />
-          <Route path="/user/" element={<MyUsers />} />
-        </Routes>
+        <div className="d-flex flex-column">
+          <div style={{
+            paddingBottom: "2.5rem"
+          }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/category/" element={<ShowCategories />} />
+              <Route path="/category/create" element={<CreateCategories />} />
+              <Route path="/recipe/" element={<MyRecipes />} />
+              <Route path="/ingredient/" element={<MyIngredients />} />
+              <Route path="/auth/" element={<Logon />} />
+              <Route path="/auth/register" element={<Register />} />
+              <Route path="/user/" element={<MyUsers />} />
+            </Routes>
+          </div>
+        <Footer className="border p-2 mt-auto" style={{
+            position: "absolute",
+            bottom: "0px",
+            left: 0,
+            right: 0,
+            width: "100%",
+            height: "2.5rem",
+            textColor: "green" 
+        }}
+        />
+        </div>
       </div>
     </>
   );
