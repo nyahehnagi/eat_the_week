2712398af8ff7f9fbadbe8ba9abd27512886bfb0
Ledders26 <benlhall584@hotmail.co.uk>
2022-04-12 22:27:46 +0100

    footer updated

--- client/src/components/team/index.js
+++ client/src/components/team/index.js
@@ -14,12 +14,12 @@ export default function Team() {
         <Col md="2">
           <h4> Shirley </h4>
           <img src="https://ca.slack-edge.com/T03ALA7H4-U02Q8GW763F-6dd1c9ecbf4e-512" className="img-fluid" alt="shirley" style={{width:"100px", height:"100px"}} />
-          <p> "" </p>
+          <p> "I'm off cycling" </p>
         </Col>
         <Col md="2">    
           <h4> Jane </h4>
           <img src="https://ca.slack-edge.com/T03ALA7H4-U02QDP72M8C-8e4f06dcb2a6-512" className="img-fluid" alt="jane" style={{width:"100px", height:"100px"}} />
-          <p> "Anyway, I'd better go and get dressed from the waist down"</p>
+          <p> "I feel like a pig in Chablis"</p>
         </Col>
         <Col md="2">  
           <h4> Bromley </h4>
