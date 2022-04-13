d4d056597295dd497d74dfb532c6bbf2091b2217
Ledders26 <benlhall584@hotmail.co.uk>
2022-04-13 13:19:07 +0100

    updated findrecipes

--- client/src/components/findrecipes/recipe.js
+++ client/src/components/findrecipes/recipe.js
@@ -21,9 +21,11 @@ export default function Recipe({ title, image, ingredients, url }) {
           window.open(url);
         }}
       />
+      <br></br>
       <button className="search-button" type="textsubmit">
         Add to Recipes
       </button>
+      <br></br>
     </div>
   );
 }
